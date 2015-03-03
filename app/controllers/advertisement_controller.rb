a3d4471a0f0f23d2d090b30558ba9495150d4244
Lillian <tanglillian1129@gmail.com>
2015-03-02 17:18:49 -0800

    CRUD assignement with migration eroor

---
+++ app/controllers/advertisement_controller.rb
@@ -0,0 +1,9 @@
+class AdvertisementController < ApplicationController
+  def index
+    @ads = Advertisement.all
+  end
+
+  def show
+    @ad = Advertisement.find(params[:id])
+  end
+end
---
+++ app/models/advertisemnt.rb
@@ -0,0 +1,3 @@
+class Advertisemnt < ActiveRecord::Base
+  
+end
