e9b158d5fda8791b881a1aa1a27425e2ba4606cc
Lillian <tanglillian1129@gmail.com>
2015-03-09 09:53:51 -0700

    avoid stashing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,8 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :comments 
   belongs_to :user 
     belongs_to :topic
 
   default_scope { order('created_at DESC') }
-=======
-  has_many :comments
-  belongs_to :user
-    belongs_to :topic
 
-  default_scope { order('created_at DESC') }
-
->>>>>>> nesting-posts
 end
