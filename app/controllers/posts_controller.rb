8c5d8c37ac13a61cf55e7f314386838d161527e0
Lillian <tanglillian1129@gmail.com>
2015-03-11 08:45:32 -0700

    authorization checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+        authorize @posts
   end
 
   def show
@@ -14,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+        authorize @post
      if @post.save
        flash[:notice] = "Post was saved."
        redirect_to @post
@@ -25,10 +27,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+        authorize @post
   end
 
    def update
      @post = Post.find(params[:id])
+        authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
        redirect_to @post
