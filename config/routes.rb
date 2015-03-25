bff20ac8a15d40cb1d83a0ff59150573097c0cac
Lillian <tanglillian1129@gmail.com>
2015-03-25 12:07:02 -0700

    voting complete finally

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,16 @@ Rails.application.routes.draw do
   resources :advertisements, :only => [:index, :show]
 
     resources :topics do
-      resources :posts, except: [:index] do
-          resources :comments, only: [:create, :destroy]
-      end
+      resources :posts, except: [:index] 
+    end  
+
+    resources :posts, only: [] do
+      resources :comments, only: [:create, :destroy]
+
+        post '/up-vote' => 'votes#up_vote', as: :up_vote
+        post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
-
-      post '/up-vote' => 'votes#up_vote', as: :up_vote
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
-
+    
   get 'about' => 'welcome#about'
  
   root to: 'welcome#index' 
