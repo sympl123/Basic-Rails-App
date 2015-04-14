c0c1759bcb107e380fcc7b2d923f6d239f99f0a6
Lillian <tanglillian1129@gmail.com>
2015-04-14 22:12:02 +0800

    devise fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325143103) do
+ActiveRecord::Schema.define(version: 20150329163258) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150325143103) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
+    t.boolean  "email_favorites"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
